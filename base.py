77dfb988a812f6f5d501f78c79a586991b317398
krish17kp <krishparekh261@gmail.com>
2025-03-08 14:05:34 +0530

    Data extract from Api

--- base.py
+++ base.py
@@ -1,2 +1,13 @@
-print("Krish Parekh")
-print("Hello coders")
+import yfinance as yf  #yahoo finance api to download daily stock indices
+import matplotlib.pyplot as plt
+ticker = yf.Ticker("^GSPC")
+price_history = ticker.history(period="max")
+if price_history.empty:
+    print("No data available for the ticker.")
+else:
+    print(price_history)
+print(price_history.columns)
+
+print(price_history.index)
+
+price_history.plot.line(y="Close",use_index=True)   
