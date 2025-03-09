98760991d69206939d69b00f79e301ae5c8fbda0
krish17kp <krishparekh261@gmail.com>
2025-03-09 09:40:34 +0530

    Added new method to get stock data

--- base.py
+++ base.py
@@ -1,11 +1,13 @@
 import yfinance as yf  #yahoo finance api to download daily stock indices
 import matplotlib.pyplot as plt
-ticker = yf.Ticker("^GSPC")
-price_history = ticker.history(period="max")
+ticker = "TATAMOTORS.NS"
+data=yf.download(ticker)
+price_history = data
 if price_history.empty:
     print("No data available for the ticker.")
 else:
     print(price_history)
+
 print(price_history.columns)
 
 print(price_history.index)
